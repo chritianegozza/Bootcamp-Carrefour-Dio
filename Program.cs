01e0d4bc35545c518499bac85f4d258e3e594470
christiane gozza <72118415+chritianegozza@users.noreply.github.com>
2022-04-07 15:58:17 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+
+Pessoa p1 = new Pessoa();
+
+p1.Nome = "Christiane";
+p1.Idade = 36;
+
+p1.EnderecoPessoa = new Endereco()
+{
+    Logradouro = "Rua Calos Gomes",
+    Numero = 350,
+    CEP = "11000000",
+    Cidade = "São Paulo",
+};
+
+WriteLine("Fim");
